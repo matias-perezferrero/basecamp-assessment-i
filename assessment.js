784857eabeebeb17e0e5d02b27605516c4404e2b
Matias Perez <matias@Matiass-MacBook-Pro-2.local>
2019-01-07 11:11:48 -0500

    completed assessment

--- assessment.js
+++ assessment.js
@@ -21,13 +21,22 @@ var trueFaveNum === faveNum;
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 // CODE HERE
-var notHakeem 
+var notHakeem = "Hakeem Olajuwan";
+function testName(name) {
+  return name==notHakeem;
+}
+testName(myName);
+
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
 var canDrive;
 
-
 // CODE HERE
+if (age>=16) {
+  return canDrive = true;
+  } else {
+  return canDrive = false;
+  }
 
 // #8 Create an if statement inside of the responseCreator function that sets the colorResponse variable value based off of a passed in value. The param value passed in will be added in the unit test. If the param value is 'green' set colorResponse variable to 'Green is okay'. If the param value is 'red' set reponse variable to 'I heart red'. Otherwise set the colorResponse variable to 'What is your favorite color?'. Make sure to return the colorResponse variable at the end once it has been properly set.
 
@@ -35,12 +44,26 @@ var colorResponse;
 
 var responseCreator = function(param){
   // CODE HERE
+  if (param=="green") {
+    return colorResponse = "Green is okay";
+  } else if (param=="red") {
+    return colorResponse = "I heart red";
+  } else {
+    return colorResponse = "What is your favorite color?";
+  }
 }
 
 // #9 Create a function called 'sum' that takes in one parameter and returns the sum of that parameter added it itself.
 
 // CODE HERE
 
+function sum (param1) {
+  return param1+=param1;
+}
+
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
 // CODE HERE
+function greeter (name,number) {
+  return "Hi! I am "+name+" and I am "+number+" years old.";
+ }
